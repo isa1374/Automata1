65466dcf937d523c3480da97fc87893df6e95e0f
isa1374 <isha13gp@hotmail.com>
2016-10-19 12:36:42 -0500

    scanner_in

--- Automata_1.1/Principal.java
+++ Automata_1.1/Principal.java
@@ -1,11 +1,17 @@
+import java.io.File;
 import java.uti.Scanner; 
 
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		File file; 
-
+		File file = new File("C:/Users/isha1_000/Documents/Isa/ITC/7to Semestre/Matemáticas Computacionales/Proyecto/PrimeraParte/A1.txt");
+		Scanner in = new Scanner(file); 
+		String line; 
+		
+		while(in.hasnextLine()){
+			
+		}
 	}
 
 }
